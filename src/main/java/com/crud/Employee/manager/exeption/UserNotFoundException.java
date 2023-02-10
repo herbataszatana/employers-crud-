73a580163bc028c49a67aa1074fb0669592e5ba1
Anna <anma.swiatkowska@gmail.com>
2023-02-10 15:37:41 +0000

    Backend

---
+++ src/main/java/com/crud/Employee/manager/exeption/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.crud.Employee.manager.exeption;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
